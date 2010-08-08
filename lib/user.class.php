09b913123ba14e64a721cbf3863c389575e248ee
tschroefel <tschroefel@7e6f556b-d683-4e40-9f27-af564fe081fd>
2010-08-08 19:42:00 +0000

    User class started

--- lib/user.class.php
+++ lib/user.class.php
@@ -1,7 +1,7 @@
 <?php
 	/******************SynFM - yet another last.fm Framework*****************/
-	/* Filename: user.class.php
-	/* Purpose: provides access to the user capabilities
+	/* Filename: album.class.php
+	/* Purpose: provides access to the album capabilities
 	/* Created: 13.05.2010
 	/*************************************************************************
 	/* Bi-Licensed under GPLv2 and 
@@ -12,9 +12,218 @@
 	/* nightraven: http://www.lastfm.de/user/Nightr4ven
 	/************************************************************************/
 	
-	class user {
-		function __construct() {
+	require_once 'api_handler.class.php';
+	
+	class album extends api_handler {
+		private $_api_key;
+		private $_username;
+		private $_password;
+
+		function __construct($api_data) {
+			$this->_api_key = $api_data['api_key'];
+			$this->_username = $api_data['user'];
+			$this->_password = $api_data['password'];
+		}
+
+		function getArtistTracks($user, $artist, $startTimestamp = false, $endTimestamp = false, $page = false) {
+			$params = array();
+			parent::setArray('method', 'user.getartisttracks', $params);
+			parent::setArray('user', $user, $params);
+			parent::setArray('artist', $artist, $params);
+			parent::setArray('startTimestamp', $startTimestamp, $params);
+			parent::setArray('endTimestamp', $endTimestamp, $params);
+			parent::setArray('page', $page, $params);
+			$tmp = parent::api_request(parent::createQueryURL($params, $this->_api_key));
 			
+			return $tmp;
+		}
+		
+		function getEvents($user) {
+			$params = array();
+			parent::setArray('method', 'user.getevents', $params);
+			parent::setArray('user', $user, $params);
+			$tmp = parent::api_request(parent::createQueryURL($params, $this->_api_key));
+			
+			return $tmp;
+		}
+		
+		function getFriends($user, $recenttracks = false, $limit = false, $page = false) {
+			$params = array();
+			parent::setArray('method', 'user.getfriends', $params);
+			parent::setArray('user', $user, $params);
+			parent::setArray('recenttracks', $recenttracks, $params);
+			parent::setArray('limit', $limit, $params);
+			parent::setArray('page', $page, $params);
+			$tmp = parent::api_request(parent::createQueryURL($params, $this->_api_key));
+			
+			return $tmp;
+		}
+		
+		function getInfo() {
+			$params = array();
+			parent::setArray('method', 'user.getinfo', $params);
+			$tmp = parent::api_request(parent::createQueryURL($params, $this->_api_key));
+			
+			return $tmp;
+		}
+		
+		function getLovedTracks($user, $limit = false, $page = false) {
+			$params = array();
+			parent::setArray('method', 'user.getlovedtracks', $params);
+			parent::setArray('user', $user, $params);
+			parent::setArray('limit', $limit, $params);
+			parent::setArray('page', $page, $params);
+			$tmp = parent::api_request(parent::createQueryURL($params, $this->_api_key));
+			
+			return $tmp;
+		}
+		
+		function getNeighbours($user, $limit = false) {
+			$params = array();
+			parent::setArray('method', 'user.getneighbours', $params);
+			parent::setArray('user', $user, $params);
+			parent::setArray('limit', $limit, $params);
+			$tmp = parent::api_request(parent::createQueryURL($params, $this->_api_key));
+			
+			return $tmp;
+		}
+		
+		function getPastEvents($user, $page = false, $limit = false) {
+			$params = array();
+			parent::setArray('method', 'user.getpastevents', $params);
+			parent::setArray('user', $user, $params);
+			parent::setArray('page', $page, $params);
+			parent::setArray('limit', $limit, $params);
+			$tmp = parent::api_request(parent::createQueryURL($params, $this->_api_key));
+			
+			return $tmp;
+		}
+		
+		function getPlaylists($user) {
+			$params = array();
+			parent::setArray('method', 'user.getplaylists', $params);
+			parent::setArray('user', $user, $params);
+			$tmp = parent::api_request(parent::createQueryURL($params, $this->_api_key));
+			
+			return $tmp;
+		}
+		
+		function getRecentStations($user, $limit = false, $page = false) {
+			throw new Exceptions('Not yet implemented');
+		}
+		
+		function getRecentTracks($user, $limit = false, $page = false) {
+			$params = array();
+			parent::setArray('method', 'user.getrecenttracks', $params);
+			parent::setArray('user', $user, $params);
+			parent::setArray('limit', $limit, $params);
+			parent::setArray('page', $page, $params);
+			$tmp = parent::api_request(parent::createQueryURL($params, $this->_api_key));
+			
+			return $tmp;
+		}
+		
+		function getRecommendedArtists() {
+			throw new Exception('Not yet implemented');
+		}
+		
+		function getRecommendedEvents($page = false, $limit = false) {
+			throw new Exception('Not yet implemented');
+		}
+		
+		function getShouts($user) {
+			$params = array();
+			parent::setArray('method', 'user.getshouts', $params);
+			parent::setArray('user', $user, $params);
+			$tmp = parent::api_request(parent::createQueryURL($params, $this->_api_key));
+			
+			return $tmp;
+		}
+		
+		function getTopAlbums($user, $period = false) {
+			$params = array();
+			parent::setArray('method', 'user.gettopalbums', $params);
+			parent::setArray('user', $user, $params);
+			parent::setArray('period', $period, $params);
+			$tmp = parent::api_request(parent::createQueryURL($params, $this->_api_key));
+			
+			return $tmp;
+		}
+		
+		function getTopArtists($user, $period = false) {
+			$params = array();
+			parent::setArray('method', 'user.gettopartists', $params);
+			parent::setArray('user', $user, $params);
+			parent::setArray('period', $period, $params);
+			$tmp = parent::api_request(parent::createQueryURL($params, $this->_api_key));
+			
+			return $tmp;
+		}
+		
+		function getTopTags($user, $limit = false) {
+			$params = array();
+			parent::setArray('method', 'user.gettoptags', $params);
+			parent::setArray('user', $user, $params);
+			parent::setArray('limit', $limit, $params);
+			$tmp = parent::api_request(parent::createQueryURL($params, $this->_api_key));
+			
+			return $tmp;
+		}
+		
+		function getTopTracks($user, $period = false) {
+			$params = array();
+			parent::setArray('method', 'user.gettoptracks', $params);
+			parent::setArray('user', $user, $params);
+			parent::setArray('period', $period, $params);
+			$tmp = parent::api_request(parent::createQueryURL($params, $this->_api_key));
+			
+			return $tmp;
+		}
+		
+		function getWeeklyAlbumChart($user, $from = false, $to = false) {
+			$params = array();
+			parent::setArray('method', 'user.getweeklyablumchart', $params);
+			parent::setArray('user', $user, $params);
+			parent::setArray('from', $from, $params);
+			parent::setArray('to', $to, $params);
+			$tmp = parent::api_request(parent::createQueryURL($params, $this->_api_key));
+			
+			return $tmp;
+		}
+		
+		function getWeeklyArtistChart($user, $from = false, $to = false) {
+			$params = array();
+			parent::setArray('method', 'user.getweeklyartistchart', $params);
+			parent::setArray('user', $user, $params);
+			parent::setArray('from', $from, $params);
+			parent::setArray('to', $to, $params);
+			$tmp = parent::api_request(parent::createQueryURL($params, $this->_api_key));
+			
+			return $tmp;
+		}
+		
+		function getWeeklyChartList($user) {
+			$params = array();
+			parent::setArray('method', 'user.getweeklychartlist', $params);
+			parent::setArray('user', $user, $params);
+			$tmp = parent::api_request(parent::createQueryURL($params, $this->_api_key));
+			
+			return $tmp;
+		}
+		
+		function getWeeklyTrackChart($user, $from = false, $to = false) {
+			$params = array();
+			parent::setArray('method', 'user.getweeklytrackchart', $params);
+			parent::setArray('user', $user, $params);
+			parent::setArray('from', $from, $params);
+			parent:.setArray('to', $to, $params);
+			$tmp = parent::api_request(parent::createQueryURL($params, $this->_api_key));
+			
+			return $tmp;
+		}
+		
+		function shout($user, $message) {
+			throw new Exception('Not yet implemented');
 		}
 	}
 ?>
