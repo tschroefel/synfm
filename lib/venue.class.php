31645f3e1fe88d795fc8eb4c694f16e5788a05bd
tschroefel <tschroefel@7e6f556b-d683-4e40-9f27-af564fe081fd>
2010-08-11 18:07:36 +0000

    Finished venue class

--- lib/venue.class.php
+++ lib/venue.class.php
@@ -12,9 +12,51 @@
 	/* nightraven: http://www.lastfm.de/user/Nightr4ven
 	/************************************************************************/
 	
-	class venue {
-		function __construct() {
+	require_once 'api_handler.class.php';
+	
+	class venue extends api_handler {
+		private $_api_key;
+		private $_username;
+		private $_password;
+		
+		function __construct($api_data) {
+                        $this->_api_key = $api_data['api_key'];
+                        $this->_username = $api_data['user'];
+                        $this->_password = $api_data['password'];
+		}
+		
+		function getEvents($venue) {
+			$params = array();
+			parent::setArray('method', 'venue.getevents', $params);
+			parent::setArray('venue', $venue, $params);
+			$url = parent::createQueryURL($params, $this->_api_key);
+			$tmp = parent::api_request($url);
 			
+			return $tmp;
+		}
+		
+		function getPastEvents($venue, $page = false, $limit = false) {
+			$params = array();
+			parent::setArray('method', 'venue.getpastevents', $params);
+			parent::setArray('venue', $venue, $params);
+			parent::setArray('page', $page, $params);
+			parent::setArray('limit', $limit, $params);
+			$url = parent::createQueryURL($params, $this->_api_key);
+			$tmp = parent::api_request($url);
+			
+			return $tmp;
+		}
+		
+		function search($venue, $page = false, $limit = false, $country = false) {
+			$params = array();
+			parent::setArray('method', 'venue.search', $params);
+			parent::setArray('venue', $venue, $params);
+			parent::setArray('page', $page, $params);
+			parent::setArray('limit', $limit, $params);
+			$url = parent::createQueryURL($params, $this->_api_key);
+			$tmp = parent::api_request($url);
+			
+			return $tmp;
 		}
 	}
 ?>
